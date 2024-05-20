59b76ec23c1f1c2e1e36c0a4ffe29344ed624565
Rotmistr-Art <tattooer74@yandex.ru>
2024-05-20 22:27:56 +0500

    Add files via upload

---
+++ home work2.py
@@ -0,0 +1,5 @@
+home_work = 12
+lead_time = 1.5
+name_course = 'Python'
+time_one_task = lead_time/home_work
+print("Курс: " + name_course +','' всего задач:'+str(home_work)+', затрачено часов:'+str(lead_time)+','' среднее время выполнения',time_one_task,'часа.')
